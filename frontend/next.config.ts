b2b2fc21cdb2bb5bbcc6280c015687a85d6d69c0
cd-janz <cd.juanzuniga@gmail.com>
2026-01-13 18:54:44 -0500

    api url base path error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,12 @@ const nextConfig: NextConfig = {
     output: "standalone",
     crossOrigin: "use-credentials",
     async rewrites(){
-        return[
+        const apiUrl = process.env.INTERNAL_API_URL || process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+
+        return [
             {
-                source: "/api/v1:path*",
-                destination: `${process.env.INTERNAL_API_URL || process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`,
+                source: "/api/v1/:path*",
+                destination: `${apiUrl}/api/v1/:path*`,
             }
         ]
     }
