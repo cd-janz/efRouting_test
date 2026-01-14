bf1783b338ca7b30be9b4c5e38520f9df5c3f2ca
cd-janz <cd.juanzuniga@gmail.com>
2026-01-13 19:21:50 -0500

    generate logs, next ain't loading env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,12 @@ const nextConfig: NextConfig = {
     output: "standalone",
     crossOrigin: "use-credentials",
     async rewrites(){
+        console.log("exist: ", process.env)
+        console.log("exist: ", process)
+        console.log("🔍 DEBUG - INICIO DEL REWRITE");
+        console.log("👉 INTERNAL_API_URL:", process.env.INTERNAL_API_URL ? "DEFINIDA" : "MISSING");
+        console.log("👉 VALOR ACTUAL:", process.env.INTERNAL_API_URL);
         const apiUrl = process.env.INTERNAL_API_URL || process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
-
         return [
             {
                 source: "/api/v1/:path*",
